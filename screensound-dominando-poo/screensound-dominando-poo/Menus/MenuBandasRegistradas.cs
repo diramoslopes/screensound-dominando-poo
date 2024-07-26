0e66904a28525fca6f8cef43ae3edcc2d4a372ec
Diogo <diogo322008@hotmail.com>
2024-07-25 21:34:15 -0300

    trabalhando com herança no c#

---
+++ screensound-dominando-poo/screensound-dominando-poo/Menus/MenuBandasRegistradas.cs
@@ -0,0 +1,22 @@
+﻿using screensound.Modelos;
+using screensound_dominando_poo.Menus;
+
+internal class MenuBandasRegistradas : Menu
+{
+    internal void Executar(Dictionary<string, Banda> bandasRegistradas)
+    {
+        {
+            Console.Clear();
+            ExibirTituloDaOpcao("Exibindo todas as bandas registradas na nossa aplicação");
+
+            foreach (string banda in bandasRegistradas.Keys)
+            {
+                Console.WriteLine($"Banda: {banda}");
+            }
+
+            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
+            Console.ReadKey();
+            Console.Clear();
+        }
+    }
+}
--- screensound-dominando-poo/screensound-dominando-poo/Program.cs
+++ screensound-dominando-poo/screensound-dominando-poo/Program.cs
@@ -53,14 +53,18 @@ internal class Program
                     RegistrarAlbum();
                     break;
                 case 3:
-                    MostrarBandasRegistradas();
+                    MenuBandasRegistradas menuBanda = new();
+                    menuBanda.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
                     break;
                 case 4:
-                    AvaliarUmaBanda();
+                    MenuAvaliarBanda menuAvaliar = new MenuAvaliarBanda();
+                    menuAvaliar.Executar(bandasRegistradas);
+                    ExibirOpcoesDoMenu();
                     break;
                 case 5:
-                    MenuExibirDetalhes menu = new MenuExibirDetalhes();
-                    menu.Executar(bandasRegistradas);
+                    MenuExibirDetalhes menuExibir = new MenuExibirDetalhes();
+                    menuExibir.Executar(bandasRegistradas);
                     ExibirOpcoesDoMenu();
                     break;
                 case -1:
@@ -113,23 +117,6 @@ internal class Program
             ExibirOpcoesDoMenu();
         }
 
-        void MostrarBandasRegistradas()
-        {
-            Console.Clear();
-            ExibirTituloDaOpcao("Exibindo todas as bandas registradas na nossa aplicação");
-
-            foreach (string banda in bandasRegistradas.Keys)
-            {
-                Console.WriteLine($"Banda: {banda}");
-            }
-
-            Console.WriteLine("\nDigite uma tecla para voltar ao menu principal");
-            Console.ReadKey();
-            Console.Clear();
-            ExibirOpcoesDoMenu();
-
-        }
-
         void ExibirTituloDaOpcao(string titulo)
         {
             int quantidadeDeLetras = titulo.Length;
@@ -138,35 +125,6 @@ internal class Program
             Console.WriteLine(titulo);
             Console.WriteLine(asteriscos + "\n");
         }
-
-        void AvaliarUmaBanda()
-        {
-            Console.Clear();
-            ExibirTituloDaOpcao("Avaliar banda");
-            Console.Write("Digite o nome da banda que deseja avaliar: ");
-            string nomeDaBanda = Console.ReadLine()!;
-            if (bandasRegistradas.ContainsKey(nomeDaBanda))
-            {
-                Banda banda = bandasRegistradas[nomeDaBanda];
-                Console.Write($"Qual a nota que a banda {nomeDaBanda} merece: ");
-                Avaliacao nota = Avaliacao.Parse(Console.ReadLine()!);
-                banda.AdicionarNota(nota);
-                Console.WriteLine($"\nA nota {nota.Nota} foi registrada com sucesso para a banda {nomeDaBanda}");
-                Thread.Sleep(2000);
-                Console.Clear();
-                ExibirOpcoesDoMenu();
-            }
-            else
-            {
-                Console.WriteLine($"\nA banda {nomeDaBanda} não foi encontrada!");
-                Console.WriteLine("Digite uma tecla para voltar ao menu principal");
-                Console.ReadKey();
-                Console.Clear();
-                ExibirOpcoesDoMenu();
-            }
-
-        }
-
         ExibirOpcoesDoMenu();
     }
 }
